1255962fa05c060d75d70cf8e8961560be0d9bb0
Valery Teplyakov <Valeriy.Teplyakov@jetbrains.com>
2020-11-30 18:35:38 +0300

    Readme + fix for console running

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,35 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 
 plugins {
-    kotlin("jvm") version "1.4.10"
-    application
+  kotlin("jvm") version "1.4.10"
+  application
 }
 
 group = "itmo.mervap"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test-junit5"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+  testImplementation(kotlin("test-junit5"))
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
 }
 
 tasks.test {
-    useJUnitPlatform()
+  useJUnitPlatform()
+  testLogging {
+    events = setOf(FAILED, PASSED)
+  }
 }
 
 tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "11"
+  kotlinOptions.jvmTarget = "11"
 }
 
 application {
-    mainClassName = "MainKt"
+  mainClassName = "itmo.mervap.hw6.MainKt"
 }
